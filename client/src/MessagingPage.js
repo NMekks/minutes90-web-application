a01c7d967abc985341509452cc4ff69a35939407
NMekks <nusaibamekka@gmail.com>
2025-08-11 03:11:46 +0300

    created component files for all the pages

---
+++ client/src/MessagingPage.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MessagingPage = () => {
+  return (
+    <View>
+      <Text>MessagingPage</Text>
+    </View>
+  )
+}
+
+export default MessagingPage
