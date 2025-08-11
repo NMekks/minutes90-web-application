2beda9b0156a1040c0df278be3d7e2b227f90819
NMekks <nusaibamekka@gmail.com>
2025-08-11 03:19:12 +0300

    added translations

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,9 @@ import App from "./App";
 import "./index.css";
 import "./App.css";
 
+// Import i18n configuration
+import './i18n';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
